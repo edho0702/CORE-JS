7c9c96f3fc19a0174cfd8c804a95015ecd9b5bbe
dongho <ldd0702@naver.com>
2024-07-03 11:15:03 +0900

    웹 컨포넌트 쉐도우돔

--- client/main.js
+++ client/main.js
@@ -2,36 +2,15 @@ class Button extends HTMLElement {
   constructor() {
     super();
 
-    this.button = document.querySelector('button');
+    this.attachShadow({ mode: 'open' });
+
+    this.shadowRoot.innerHTML = `
+      <button>hello</button>`;
   }
 
-  connectedCallback() {
-    this._render();
-  }
+  connectedCallback() {}
 
   disconnectedCallback() {}
-
-  static get observedAttributes() {
-    return ['id'];
-  }
-
-  attributeChangedCallback(name, oldValue, newValue) {
-    if (oldValue !== newValue) {
-      this._render();
-    }
-  }
-
-  _render() {
-    this.button.textContent = this.id;
-  }
 }
 
 customElements.define('c-button', Button);
-
-const c = document.querySelector('c-button');
-
-let count = 0;
-
-c.addEventListener('click', () => {
-  c.setAttribute('id', ++count);
-});
